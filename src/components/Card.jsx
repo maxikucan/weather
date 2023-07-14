7bd236b2ccb097125d56b21a34b742ad2935e795
Maximiliano Kucan <maximilianokucan@gmail.com>
2023-07-14 18:07:06 -0300

    deleted unused classnames

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/prop-types */
 function Card(props) {
   return (
-    <div className="card">
-      <div className="card-body">
-        <h4 className="card-title">{props.city}</h4>
-        <h3 className="card-title">{props.weather} C°</h3>
-        <h5 className="card-title">{props.region}</h5>
-        <h5 className="card-text">{props.country}</h5>
+    <div>
+      <div>
+        <h4>{props.city}</h4>
+        <h3>{props.weather} C°</h3>
+        <h5>{props.region}</h5>
+        <h5>{props.country}</h5>
         <small>{props.time}</small>
       </div>
     </div>
